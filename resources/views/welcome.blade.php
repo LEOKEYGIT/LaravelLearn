c0ba8de09ce77f7b6c1b016adc732e2c80ba9a1b
Leokey <815076795@qq.com>
2020-02-27 13:57:35 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>my First Laravel</title>
+        <style>
+            html,body{
+                height: 100%;
+            }
+            body{
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: "Helvetica NEVE",NotoSansHans-Regular,AvenirNext-Regular,Arial,Hiragino Sans GB,"Microsoft YaHei","Hiragino Sans GB","WenQuanYi Micro Hei",sans-serif;
+                color: #777;
+            }
+            .container{
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+            .content{
+                text-align: center;
+                display: inline-block;
+            }
+            .title{
+                font-size: 66px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">
+                    Hello Laravel! - by Leokey
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
