0f253b75adbad90835de49a83ca59eeaa0eded2c
Leokey <815076795@qq.com>
2020-02-28 19:03:44 +0800

    错误提示信息

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,7 +19,7 @@ class UsersController extends Controller
     {
         $this->validate($request,[
             'name' => 'required|unique:users|max:50',
-            'email'=>'required|email|unique:users|max:255',
+            'email'=>'required|email|unique:email|max:255',
             'password'=>'required|confirmed|min:6'
         ]);
         return;
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,32 +8,40 @@
                 <h5>注冊</h5>
             </div>
             <div class="card-body">
+
+                @include('shared._errors')
+
                 <form method="Post" action="{{ route('users.store') }}">
-                    {{ csrf_token() }}
+                    {{ csrf_field() }}
+
                     <div class="form-group">
                         <label for="name">
                             名稱：
                         </label>
                         <input type="text" name="name" class="form-control" value="{{ old('name') }}">
                     </div>
+
                     <div class="form-group">
                         <label for="name">
                             郵箱：
                         </label>
                         <input type="text" name="email" class="form-control" value="{{ old('email') }}">
                     </div>
+
                     <div class="form-group">
                         <label for="name">
                             密碼：
                         </label>
-                        <input type="text" name="password" class="form-control" value="{{ old('password') }}">
+                        <input type="password" name="password" class="form-control" value="{{ old('password') }}">
                     </div>
+
                     <div class="form-group">
                         <label for="name">
                             確認密碼：
                         </label>
-                        <input type="text" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+                        <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
                     </div>
+
                     <button type="submit" class="btn btn-primary">注冊</button>
                 </form>
             </div>
